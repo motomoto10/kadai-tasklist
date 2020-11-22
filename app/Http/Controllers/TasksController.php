143c0b2e38c95aa551c7906b55ca30e6b15e88c0
takuro.moto <kpdvox8370@yahoo.co.jp>
2020-11-22 11:11:45 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -43,8 +43,14 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+    {   
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255'
+            ]);
+        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -91,9 +97,15 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255'
+            ]);
+        
         $task = Task::findOrFail($id);
         
-        $task->content =$request->content;
+        $task->status = $request->status;
+        $task->content = $request->content;
         $task->save();
         
         return redirect('/');
